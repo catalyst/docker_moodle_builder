b65966fcb49b3af926b34a7425aea748b6b10adc
Kenneth Hendricks <ken.c.hendricks@gmail.com>
2019-07-13 12:17:36 +1000

    Add basic build process that will add a readme

--- build.py
+++ build.py
@@ -1,1 +1,29 @@
-print("placeholder")
+import os
+from shutil import copyfile, rmtree
+
+def buildBranch(distro, db):
+    branchName = "{0}-{1}".format(distro, db)
+    print("Building {0}".format(branchName))
+
+    # Create new branch folder
+    dirName = os.path.dirname(__file__)
+    branchFolder = os.path.join(dirName, "build/{0}".format(branchName))
+
+    if os.path.isdir(branchFolder):
+        rmtree(branchFolder)
+    os.mkdir(branchFolder)
+
+    #figure out asset path
+    assetPath = os.path.join(dirName, "assets")
+
+    # Copy in static README
+    readmePath = os.path.join(assetPath, "static/README.md")
+    copyfile(readmePath, os.path.join(branchFolder, "README.md"))
+
+
+ubuntuDistributions = ['1404', '1604', '1804']
+databases = ['mysql', 'psql']
+
+for distro in ubuntuDistributions:
+    for db in databases:
+        buildBranch(distro, db)
