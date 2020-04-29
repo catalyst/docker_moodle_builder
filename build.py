e5ba47d9a15bbe8a7fb2824b92298dfa5455a282
Kenneth Hendricks <kennethhendricks@catalyst-au.net>
2020-04-29 13:22:39 +1000

    Format build.py

--- build.py
+++ build.py
@@ -3,6 +3,7 @@ import shutil
 import git
 import jinja2
 
+
 def createBranchFolder(branchPath):
     if os.path.isdir(branchPath):
         shutil.rmtree(branchPath)
@@ -14,6 +15,7 @@ def createBranchFolder(branchPath):
     dockerMoodlePath = os.path.join(dockerPath, "moodle")
     os.mkdir(dockerMoodlePath)
 
+
 def addStaticFile(assetPath, branchPath, fileName, outputName=None):
     if outputName is None:
         outputName = fileName
@@ -24,16 +26,18 @@ def addStaticFile(assetPath, branchPath, fileName, outputName=None):
 
 def addTemplatedFile(assetPath, branchPath, templateName, templateData):
     templateLoader = jinja2.FileSystemLoader(searchpath=assetPath)
-    templateEnv = jinja2.Environment(loader=templateLoader, trim_blocks=True, lstrip_blocks=True)
+    templateEnv = jinja2.Environment(
+        loader=templateLoader, trim_blocks=True, lstrip_blocks=True
+    )
     template = templateEnv.get_template("{0}.j2".format(templateName))
     outputPath = os.path.join(branchPath, templateName)
 
-    with open(outputPath, 'w') as f:
-            f.write(template.render(templateData))
+    with open(outputPath, "w") as f:
+        f.write(template.render(templateData))
 
 
 def buildBranch(ubuntuVersion, db):
-    branchName = "{0}-{1}".format(ubuntuVersion['name'], db['name'])
+    branchName = "{0}-{1}".format(ubuntuVersion["name"], db["name"])
     print("Building {0}".format(branchName))
 
     dirName = os.path.dirname(__file__)
@@ -42,154 +46,134 @@ def buildBranch(ubuntuVersion, db):
 
     createBranchFolder(branchPath)
 
-    assetPath = os.path.join(dirName, 'assets')
+    assetPath = os.path.join(dirName, "assets")
 
-    addStaticFile(assetPath, branchPath, 'README.md')
-    addStaticFile(assetPath, branchPath, 'control')
-    os.chmod(os.path.join(branchPath, 'control'), 0o775)
+    addStaticFile(assetPath, branchPath, "README.md")
+    addStaticFile(assetPath, branchPath, "control")
+    os.chmod(os.path.join(branchPath, "control"), 0o775)
 
-    addStaticFile(assetPath, branchPath, 'gitignore', '.gitignore')
-    addStaticFile(assetPath, dockerMoodlePath, 'nginx.conf')
-    addStaticFile(assetPath, dockerMoodlePath, 'xdebug.ini')
+    addStaticFile(assetPath, branchPath, "gitignore", ".gitignore")
+    addStaticFile(assetPath, dockerMoodlePath, "nginx.conf")
+    addStaticFile(assetPath, dockerMoodlePath, "xdebug.ini")
 
-    packages = ['curl', 'locales', 'nginx', 'vim']
-    packages += ubuntuVersion['packages']
-    packages += db['packages'][ubuntuVersion['name']]
+    packages = ["curl", "locales", "nginx", "vim"]
+    packages += ubuntuVersion["packages"]
+    packages += db["packages"][ubuntuVersion["name"]]
     packages.sort()
 
     addTemplatedFile(
         assetPath,
         dockerMoodlePath,
-        'Dockerfile',
+        "Dockerfile",
         {
-            'imageTag': ubuntuVersion['imageTag'],
-            'packages': packages,
-            'xdebugPath': ubuntuVersion['xdebugPath'],
-        }
+            "imageTag": ubuntuVersion["imageTag"],
+            "packages": packages,
+            "xdebugPath": ubuntuVersion["xdebugPath"],
+        },
     )
 
     addTemplatedFile(
         assetPath,
         dockerMoodlePath,
-        'entrypoint.sh',
-        {
-            'fpmService': ubuntuVersion['fpmService'],
-        }
+        "entrypoint.sh",
+        {"fpmService": ubuntuVersion["fpmService"],},
     )
 
     addTemplatedFile(
         assetPath,
         dockerMoodlePath,
-        'nginx-site',
-        {
-            'fpmSock': ubuntuVersion['fpmSock'],
-        }
+        "nginx-site",
+        {"fpmSock": ubuntuVersion["fpmSock"],},
     )
 
-    addTemplatedFile(
-        assetPath,
-        branchPath,
-        'docker-compose.yml',
-        {
-            'db': db['name']
-        }
-    )
+    addTemplatedFile(assetPath, branchPath, "docker-compose.yml", {"db": db["name"]})
 
-    addTemplatedFile(
-        assetPath,
-        branchPath,
-        'moodle-config',
-        {
-            'db': db['name']
-        }
-    )
+    addTemplatedFile(assetPath, branchPath, "moodle-config", {"db": db["name"]})
 
 
 ubuntuVersions = [
     {
-        'name':         '1404',
-        'imageTag':     '14.04',
-        'fpmService':   'php5-fpm',
-        'fpmSock':      '/var/run/php5-fpm.sock',
-        'xdebugPath':   '/etc/php5/mods-available/xdebug.ini',
-        'packages':     [
-            'php5',
-            'php5-cli',
-            'php5-curl',
-            'php5-fpm',
-            'php5-gd',
-            'php5-intl',
-            'php5-ldap',
-            'php5-xdebug',
-            'php5-xmlrpc',
-            'php-soap',
+        "name": "1404",
+        "imageTag": "14.04",
+        "fpmService": "php5-fpm",
+        "fpmSock": "/var/run/php5-fpm.sock",
+        "xdebugPath": "/etc/php5/mods-available/xdebug.ini",
+        "packages": [
+            "php5",
+            "php5-cli",
+            "php5-curl",
+            "php5-fpm",
+            "php5-gd",
+            "php5-intl",
+            "php5-ldap",
+            "php5-xdebug",
+            "php5-xmlrpc",
+            "php-soap",
         ],
     },
-
     {
-        'name':         '1604',
-        'imageTag':     '16.04',
-        'fpmService':   'php7.0-fpm',
-        'fpmSock':      '/var/run/php/php7.0-fpm.sock',
-        'xdebugPath':   '/etc/php/7.0/mods-available/xdebug.ini',
-        'packages':     [
-            'php',
-            'php-cli',
-            'php-curl',
-            'php-fpm',
-            'php-gd',
-            'php-intl',
-            'php-ldap',
-            'php-mbstring',
-            'php-mysql',
-            'php-soap',
-            'php-xdebug',
-            'php-xmlrpc',
-            'php-xml',
-            'php-zip',
+        "name": "1604",
+        "imageTag": "16.04",
+        "fpmService": "php7.0-fpm",
+        "fpmSock": "/var/run/php/php7.0-fpm.sock",
+        "xdebugPath": "/etc/php/7.0/mods-available/xdebug.ini",
+        "packages": [
+            "php",
+            "php-cli",
+            "php-curl",
+            "php-fpm",
+            "php-gd",
+            "php-intl",
+            "php-ldap",
+            "php-mbstring",
+            "php-mysql",
+            "php-soap",
+            "php-xdebug",
+            "php-xmlrpc",
+            "php-xml",
+            "php-zip",
         ],
     },
-
     {
-        'name':         '1804',
-        'imageTag':     '18.04',
-        'fpmService':   'php7.2-fpm',
-        'fpmSock':      '/var/run/php/php7.2-fpm.sock',
-        'xdebugPath':   '/etc/php/7.2/mods-available/xdebug.ini',
-        'packages': [
-            'php',
-            'php-cli',
-            'php-curl',
-            'php-fpm',
-            'php-gd',
-            'php-intl',
-            'php-ldap',
-            'php-mbstring',
-            'php-mysql',
-            'php-soap',
-            'php-xdebug',
-            'php-xmlrpc',
-            'php-xml',
-            'php-zip',
+        "name": "1804",
+        "imageTag": "18.04",
+        "fpmService": "php7.2-fpm",
+        "fpmSock": "/var/run/php/php7.2-fpm.sock",
+        "xdebugPath": "/etc/php/7.2/mods-available/xdebug.ini",
+        "packages": [
+            "php",
+            "php-cli",
+            "php-curl",
+            "php-fpm",
+            "php-gd",
+            "php-intl",
+            "php-ldap",
+            "php-mbstring",
+            "php-mysql",
+            "php-soap",
+            "php-xdebug",
+            "php-xmlrpc",
+            "php-xml",
+            "php-zip",
         ],
     },
 ]
 
 databases = [
     {
-        'name': 'mysql',
-        'packages': {
-            '1404': ['php5-mysql'],
-            '1604': ['php-mysql'],
-            '1804': ['php-mysql'],
+        "name": "mysql",
+        "packages": {
+            "1404": ["php5-mysql"],
+            "1604": ["php-mysql"],
+            "1804": ["php-mysql"],
         },
     },
     {
-        'name': 'psql',
-        'packages': {
-            '1404': ['php5-pgsql'],
-            '1604': ['php-pgsql'],
-            '1804': ['php-pgsql'],
+        "name": "psql",
+        "packages": {
+            "1404": ["php5-pgsql"],
+            "1604": ["php-pgsql"],
+            "1804": ["php-pgsql"],
         },
     },
 ]
